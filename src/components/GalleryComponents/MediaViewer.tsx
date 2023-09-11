548f9b4d8448bff09081561de3fb8ec5810ee3aa
Sokol Paja <kolpaja28@gmail.com>
2023-09-11 10:48:48 +0200

    remove autoplay

--- src/components/GalleryComponents/MediaViewer.tsx
+++ src/components/GalleryComponents/MediaViewer.tsx
@@ -5,7 +5,7 @@ const MediaViewer = ({ mediaObject }: any) => {
     return <img src={link} alt="Image" crossOrigin="anonymous" />
   } else if (type === "video/mp4") {
     return (
-      <video controls autoPlay width="720" height={mediaObject.height}>
+      <video controls width="720" height={mediaObject.height}>
         <source src={link} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
